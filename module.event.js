896a42fefb6756aab585504261c68b201530537f
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-01-17 10:49:29 +0900

    update daily and event

--- module.event.js
+++ module.event.js
@@ -1,24 +1,18 @@
-// event
 
-// process 객체에 exit 이벤트 연결
-process.on('exit', function(){
-    console.log('BYE!!!!!-----');
-});
 
-// process 객체에 uncauchtException 이벤트 연결
-process.on('uncaughtException', function(error){
-    console.log('EXCEPTION !!!!!!@@@@!!!!@@@@');
-});
 
-// 2초 간격으로 3번 예외를 발생
-var count = 0;
-var id = setInterval(function(){
-    count++;
+// 2. remove listener
+var onUncaughtException = function(error){
+    console.log('exception !!!');
 
-    if(count == 3){
-        clearInterval(id);
-    }
+    // remove event
+    process.removeListener('uncaughtException', onUncaughtException);
+};
 
-    // excetpion 강제 발생
-    error.error.error();
+// connect event
+process.on('uncaughtException', onUncaughtException);
+
+// 2초마다 함수 실행
+setInterval(function(){
+    error.error.error('^-^');
 },2000);
