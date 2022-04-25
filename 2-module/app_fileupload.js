477f8bbdb1a23bede10c7b8fde3074023f0db7f0
sehooh5 <58541635+sehooh5@users.noreply.github.com>
2022-04-25 11:35:49 +0900

    update fileupload

--- 2-module/app_fileupload.js
+++ 2-module/app_fileupload.js
@@ -24,8 +24,36 @@ app.get('/', function(request, response){
 app.post('/', function(request, response){
     console.log(request.body);
     console.log(request.files);
+    // 파일의 이름 변경
+    var comment = request.param('comment');
+    var imageFile = request.files.image;
 
-    response.redirect('/');
+    if(imageFile){
+        var name = imageFile.name;
+        var path = imageFile.path;
+        var type = imageFile.type;
+
+
+        // 이미지 파일 확인
+        if(type.indexOf('image') != -1){
+            // 이미지 파일의 경우 : 파일 이름을 변경
+            var outputPath = __dirname + '/multipart/' + Date.now() + '_' + name;
+
+            fs.rename(path, outputPath, function(error){
+                response.redirect('/');
+            });
+        } else {
+            // 이미지 파일이 아닐 경우 : 파일 이름을 제거
+            fs.unlink(path, function(error){
+                response.send(400);
+            });
+        }
+    } else {
+        // 파일이 없는 경우
+        response.send(404);
+    }
+
+
 });
 
 http.createServer(app).listen(52273, function(){
